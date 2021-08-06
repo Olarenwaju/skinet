0b0500b60e38b9c457aefe17aab3bfe5381b56b3
Olarenwaju <muberium516@gmail.com>
2021-08-06 17:09:24 +0100

    Paging, Filtering,Sorting and Searching.

--- API/Startup.cs
+++ API/Startup.cs
@@ -20,7 +20,7 @@ namespace API
             _config = config;
         }
 
-
+ 
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
@@ -33,6 +33,13 @@ namespace API
 
             services.AddApplicationServices();
             services.AddSwaggerDocumentation();
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+                });
+            });
   
         }
 
@@ -49,6 +56,8 @@ namespace API
             app.UseRouting();
             
             app.UseStaticFiles();
+
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
